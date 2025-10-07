7cd006826aace84ee28f76deea3067ae5178bf40
alpatlab-lab <alpatlab@gmail.com>
2025-10-07 13:24:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolsapa = document.getElementByld('sapabutton');
-tombolsapa.addEventListener('click',function(){
-  alert('halo! selamat barkunjung di link saya!');
+const tombolsapa = document.getElementById('sapabutton'); // Huruf 'I' kapital, bukan 'l'
+tombolsapa.addEventListener('click', function() {
+  alert('halo! selamat berkunjung di link saya!');
 });
